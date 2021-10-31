c67e4e6173366a844ce0c8575061800795877d03
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-10-31 16:57:11 +0200

    pagination functionality has implemented

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config.js';
+import { API_URL, RESULTS_PER_PAGE } from './config.js';
 import { getJSON } from './helpers.js';
 
 export const state = {
@@ -6,6 +6,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    page: 1,
+    resultsPerPage: RESULTS_PER_PAGE,
   },
 };
 
@@ -44,4 +46,11 @@ export const loadSearchResult = async function (query) {
     console.error(err.message);
     throw err;
   }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = start + state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
 };
