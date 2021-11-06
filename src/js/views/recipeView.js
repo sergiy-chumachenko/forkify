85cf49fb3e192954ce27a885efa3507bfbd5772b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2021-11-06 13:00:03 +0200

    remove fraction from recipe ingredients

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+// import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -125,7 +125,8 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            // ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            ing.quantity
           }</div>
           <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
